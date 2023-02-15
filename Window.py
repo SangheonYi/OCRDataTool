212e805627b41ea5abfcdaba31556eaa19ee7e32
SangheonYi <tmzpdpdlwl@gmail.com>
2023-02-15 15:18:31 +0900

    feat: Feat splitter ui

--- Window.py
+++ Window.py
@@ -1,33 +1,54 @@
 from PyQt6.QtWidgets import QMainWindow, QMenu, QFileDialog
-from TrainDataViewer import TrainDataViewer
+from TrainDataView import TrainDataView
 from PyQt6.QtGui import QAction
 from PyQt6.QtCore import pyqtSlot, Qt
+from PyQt6 import uic
 from pathlib import Path
 
-class MainWindow(QMainWindow):
+form_class = uic.loadUiType("ocr_data_tool.ui")[0]
+
+class MainWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.scaleFactor = 0.0
         self.logList = []
         self.currentImageIndex = -1
         self.viewPathBool = False
-
-        self.trainDataViewer = TrainDataViewer()
-        self.setCentralWidget(self.trainDataViewer)
+        self.trainDataView = TrainDataView(self)
+        self.setCentralWidget(self.imageTopSplitter)
+        self.imageTopSplitter.setSizes([400, 1200])
 
         self.createActions()
         self.createMenus()
-        self.setWindowTitle("Image Viewer")
+        self.setWindowTitle("Image View")
         self.resize(1900, 1600)
+
+        # for debug
+        fileName = 'parsed_inf.log'
+        with open(fileName, 'r', encoding='utf-8') as logFile:
+            rawLogList = logFile.readlines()
+            for idx in range(len(rawLogList) // 5):
+                log4Lines =  rawLogList[5 * idx : 5 * (idx + 1)]
+                croppedPath = log4Lines[0][:-1].split('path: ')[1]
+                logGt = ''.join(log4Lines[1:])
+                self.logList.append((croppedPath, logGt))
+        self.nextImage()
+        self.scaleFactor = 1.0
+
+        self.fitToWindowAct.setEnabled(True)
+        self.updateActions()
     @pyqtSlot()
-    def open(self):
+    def openLog(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Open logfile", ".", filter="inference *.log")
         if fileName:
+            pdfList = set()
             with open(fileName, 'r', encoding='utf-8') as logFile:
                 rawLogList = logFile.readlines()
                 for idx in range(len(rawLogList) // 5):
                     log4Lines =  rawLogList[5 * idx : 5 * (idx + 1)]
                     croppedPath = log4Lines[0][:-1].split('path: ')[1]
+                    pdfList.add(croppedPath.split('\\'))
                     logGt = ''.join(log4Lines[1:])
                     self.logList.append((croppedPath, logGt))
             self.nextImage()
@@ -51,7 +72,7 @@ class MainWindow(QMainWindow):
         self.menuBar().addMenu(self.viewMenu)
 
     def createActions(self):
-        self.openAct = QAction("Open...", self, shortcut="Ctrl+O", triggered=self.open)
+        self.openAct = QAction("Open...", self, shortcut="Ctrl+O", triggered=self.openLog)
         self.zoomInAct = QAction("Zoom In (25%)", self, shortcut="Ctrl++", enabled=False, triggered=self.zoomIn)
         self.zoomOutAct = QAction("Zoom Out (25%)", self, shortcut="Ctrl+-", enabled=False, triggered=self.zoomOut)
         self.normalSizeAct = QAction("Normal Size", self, shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
@@ -65,12 +86,12 @@ class MainWindow(QMainWindow):
         elif key==Qt.Key.Key_Left.value:
             self.prevImage()
         elif key==Qt.Key.Key_O.value:
-            self.trainDataViewer.croppedImageViewer.viewOutSourcing()
+            self.trainDataView.croppedImageView.viewOutSourcing()
         elif key==Qt.Key.Key_P.value:
             self.viewPathBool = not self.viewPathBool
-            self.trainDataViewer.croppedImageViewer.setNewText(self.viewPathBool)
+            self.trainDataView.croppedImageView.setNewText(self.viewPathBool)
         elif key in [Qt.Key.Key_C.value, Qt.Key.Key_B.value]:
-            self.trainDataViewer.croppedImageViewer.copyToClipboard(key)
+            self.trainDataView.croppedImageView.copyToClipboard(key)
         else:
             print(event)
 
@@ -79,11 +100,15 @@ class MainWindow(QMainWindow):
         croppedPath, gt = self.logList[self.currentImageIndex]
         croppedPath = Path(croppedPath)
         boxedPageIdx = int(Path(croppedPath).stem.split('_')[0])
-        boxedDir = Path(str(croppedPath.parent).replace('cropped', 'boxed'))
+        ## for debug
+        wsl_path = Path("Z:\home\sayi\workspace\OCR\TrainDataPreprocess\PDFPreprocess\\")
+        ## 
+        boxedDir = wsl_path / Path(str(croppedPath.parent).replace('cropped', 'boxed'))
+
         boxedPath = sorted([str(itered_path) for itered_path in boxedDir.iterdir()])[boxedPageIdx] # sort key 지정 안 해서 멋대로 정렬되면 페이지 잘못 매핑될 수
         croppedPath = str(croppedPath)
         gt = '\n'.join([gt, croppedPath, boxedPath])
-        self.trainDataViewer.setNewImage(boxedPath, croppedPath, gt, self.viewPathBool)
+        self.trainDataView.setNewImage(boxedPath, croppedPath, gt, self.viewPathBool)
 
     def nextImage(self):
         if self.currentImageIndex + 1 < len(self.logList):
@@ -136,6 +161,6 @@ if __name__ == '__main__':
     from PyQt6.QtWidgets import QApplication
 
     app = QApplication(sys.argv)
-    imageViewer = MainWindow()
-    imageViewer.show()
+    imageView = MainWindow()
+    imageView.show()
     sys.exit(app.exec())
